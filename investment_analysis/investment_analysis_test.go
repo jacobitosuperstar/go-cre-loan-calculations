9b3fff529bbfadbe634ed1ffa0d2daad84e96ffb
Jacobo Bedoya <Jacobo.Bedoya@endava.com>
2024-11-24 01:18:31 -0500

    FEAT: Started fixing the tests.

--- investment_analysis/investment_analysis_test.go
+++ investment_analysis/investment_analysis_test.go
@@ -8,64 +8,57 @@ import (
 
 func TestROI(t *testing.T) {
     var testCases = []struct {
-        input  ReturnOfInvestment
+        input  map[string]interface{}
         // want  []map[string]interface{}
     }{
       {
-        ReturnOfInvestment{
-          taxMetrics: TaxAssumptions{
-            LanBuildingValue: 0.3,
-            FixDepreciationTimeLine: 27,
-            IncomeTaxRate: 0.25,
-            CapitalGainsTaxRate: 0.15,
-            DepreciationRecaptureTaxRate: 0.25,
+          map[string]interface{} {
+              "purchase_price": 0,
+              "closing_an_renovations": 0,
+              "going_in_cap_rate": 0,
+              "initial_revenue": 0,
+              "initial_expenses": 0,
+              "initial_capital_reserves": 0,
+              "projected_revenue_growth": 0,
+              "projected_expenses_growth": 0,
+              "projected_capital_reserves_growth": 0,
+              // LoanSizer
+              "max_ltv": 0,
+              "min_dscr": 0,
+              "amortization": 0,
+              "term": 0,
+              "interest_rate": 0,
+              "io_period": 0,
+              "requested_loan_amount": 0,
+              "loan_origination_fees": 0,
+              // TaxAssumptions
+              "lan_building_value": 0,
+              "fix_depreciation_time_line": 0,
+              "income_tax_rate": 0,
+              "capitalGains_tax_rate": 0,
+              "depreciation_recapture_tax_rate": 0,
+              // SaleTerms
+              "exit_cap_rate": 0,
+              "cost_of_sale": 0,
+              "sale_year": 0,
           },
-          dealMetrics: DealInformation{
-            PurchasePrice: 6500000,
-            ClosingAndRenovations: 225000,
-            GoingInCapRate: 0.0596,
-            InitRevenue: 687500,
-            InitOperatingExpenses: 300000,
-            InitCapitalReserves: 7500,
-            ProjRevenueGrowth: 0.0350,
-            ProjOperatingExpensesGrowth: 0.0250,
-            ProjCapitalReservesGrowth: 0.0250,
-          },
-          loanMetrics: ls.LoanSizer{
-            MaxLTV: 0.70,
-            MinDSCR: 1.25,
-            Amortization: 30,
-            Term: 10,
-            IOPeriod: 2,
-            Rate: 0.0450,
-            PropertyValue: 10000000,
-            NOI: 0.0,
-            RequestedLoanAmount: 10000000,
-            LoanOriginationFees: 0.01,
-          },
-          saleMetrics: SaleTerms{
-            ExitCapRate: 0.0650,
-            CostOfSale: 0.0250,
-            SaleYear: 10,
-          },
-        },
       },
     }
 
     for _, test := range testCases {
-      net_cash_flow_projection, err := test.input.NetCashFlowProjection()
-      log.Printf("roi: %+v\n", net_cash_flow_projection)
+      roi, err := NewReturnOfInvestment()
       if err != nil {
-          t.Errorf("NetCashFlowProjection got: %v, error: %v", net_cash_flow_projection, err)
+          t.Errorf("ReturnOfInvestment internal error: %v", err)
           return
       }
+      log.Printf("roi: %v",roi)
       // log.Printf("roi: %+v\n", net_cash_flow_projection)
-      t.Errorf("got: %v", net_cash_flow_projection)
-      ppmt, ipmt, err := test.input.loanMetrics.PaymentDistribution()
-      if err != nil {
-          t.Errorf("NetCashFlowProjection error: %v", err)
-          return
-      }
-      t.Errorf("LOOK HERE ppmt: %v, ipmt: %v", ppmt, ipmt)
+      // t.Errorf("got: %v", net_cash_flow_projection)
+      // ppmt, ipmt, err := roi.loanMetrics.PaymentDistribution()
+      // if err != nil {
+      //     t.Errorf("NetCashFlowProjection error: %v", err)
+      //     return
+      // }
+      // t.Errorf("LOOK HERE ppmt: %v, ipmt: %v", ppmt, ipmt)
     }
 }
